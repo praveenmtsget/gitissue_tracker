d3f79383e16b1c5dfddd79b7739cd038bef67102
Praveen Nin <p.ninduj@gmail.com>
2024-05-23 23:23:04 -0400

    webhook integration and dockerfile

--- app.py
+++ app.py
@@ -30,6 +30,29 @@ def fetch_issues():
 
     return render_template('index.html', issues=issues)
 
+# webhook logic
+webhook_issues = []  # Store new issues received through webhook
 
+@app.route('/webhook', methods=['POST', 'GET'])
+def webhook():
+
+    if (request.method == 'POST'):
+        data = request.json
+        issue = data.get('issue')
+
+        if issue:
+            webhook_issues.append(issue)
+
+        return 'Webhook received'
+    elif (request.method == 'GET'):
+        print(webhook_issues)
+        response_html = ""
+        for issue in webhook_issues:
+            print(issue)
+            response_html += f'{issue["title"]} - {issue["state"]}'
+        return response_html
+
+        
+        
 if __name__ == '__main__':
     app.run(debug=True)
